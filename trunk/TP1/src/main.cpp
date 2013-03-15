27b516ab2a68333ff348ccd13a751408b6c89310
incorporado <incorporado@3db02732-8920-3d1c-915d-0703c299baa4>
2013-03-15 18:44:15 +0000

    que miercole es onuolbaid???

--- trunk/TP1/src/main.cpp
+++ trunk/TP1/src/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[]){
 	std::cout << "Mart -> SVN... Ok!" << std::endl;
 	std::cout << "" << std::endl;
 	std::cout << "" << std::endl;
+	std::cout << "Dany -> SVN... Ok!" << std::endl;
 	std::cout << "" << std::endl;
 	std::cout << "" << std::endl;
 	std::cout << "Seba -> SVN... Ok!" << std::endl;
