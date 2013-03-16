8b0760b7bec8b26eb3fd9acfb7e152de81314b40
incorporado <incorporado@3db02732-8920-3d1c-915d-0703c299baa4>
2013-03-16 21:15:53 +0000

    probé y SDL OK

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[]){
 
 	std::cout << "Mart -> SVN... Ok!" << std::endl;
 	std::cout << "Dany -> SVN... Ok!" << std::endl;
+	std::cout << "Dany -> SDL... Ok!" << std::endl;
 	std::cout << "Seba -> SVN... Ok!" << std::endl;
 	std::cout << "Matias -> SVN... Ok!" << std::endl;
 	std::cout << "" << std::endl;
