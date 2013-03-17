3ba01ddb64a27442f9247a56a8f7cd48868deba9
martineq@gmail.com <martineq@gmail.com@3db02732-8920-3d1c-915d-0703c299baa4>
2013-03-17 00:23:34 +0000

    + VLD agregado y funcionando Ok.-

--- trunk/TP1/src/main.cpp
+++ trunk/TP1/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "./utils/Pruebas/Pruebas.h"
-#include <vld.h> // >> Sólo descomentar para chequeo de memoria. Habilitar DLL de depuración multiproceso (/MDd).
+//#include <vld.h> // >> Sólo descomentar para chequeo de memoria. Habilitar DLL de depuración multiproceso (/MDd).
 
 
 int main(int argc, char* argv[]){
