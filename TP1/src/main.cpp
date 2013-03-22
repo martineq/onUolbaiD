61034023cacb6dde5172983b91a7b9df664ad765
martineq <martineq@gmail.com>
2013-03-22 21:04:48 +0000

    + La clase ParserYaml ya lee todos los datos y detecta errores sintácticos. Falta el chequeo de errores semánticos.-

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -4,34 +4,11 @@
 
 int main(int argc, char* argv[]) {
 
-	std::cout << "Mart -> SVN... Ok!" << std::endl;
-	std::cout << "Mart -> SDL... Ok!" << std::endl;
-	std::cout << "Mart -> VLD... Ok!" << std::endl;
-	std::cout << "Mart -> YAML... Ok!" << std::endl;
-
-	std::cout << "Dany -> SVN... Ok!" << std::endl;
-	std::cout << "Dany -> SDL... Ok!" << std::endl;
-	std::cout << "" << std::endl;
-	std::cout << "" << std::endl;
-
-	std::cout << "Seba -> SVN... Ok!" << std::endl;
-	std::cout << "Seba -> SDL... Ok!" << std::endl;
-	std::cout << "Seba -> VLD... Ok!" << std::endl;
-	std::cout << "" << std::endl;
-
-	std::cout << "Matias -> SVN... Ok!" << std::endl;
-	std::cout << "Matias -> SDL... Ok!" << std::endl;
-	std::cout << "Matias -> VLD... Ok!" << std::endl;
-	std::cout << "Matias -> YAML... Ok!" << std::endl;
-
 	Pruebas p;
-	p.PruebaSdl();	// Prueba SDL + log
+	//p.PruebaSdl();	// Prueba SDL + log
 	p.PruebaYAML();	// Prueba YAML
-
-	char c;
-	std::cin >> c;
-
-	char* puntero = new char;	// Creo un puntero y nunca lo saco de memoria dinámica. Para ver si me lo detecta el VLD.
 
 	return 0;
 }
+
+
