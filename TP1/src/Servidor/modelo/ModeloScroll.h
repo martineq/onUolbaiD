67856012ff773ecfb6fb4d8d14611dce1c319937
seba3789 <seba3789@3db02732-8920-3d1c-915d-0703c299baa4>
2013-03-27 02:17:37 +0000

    Subo lo que tengo de ModeloScroll. No está terminado, ya que tenemos que definir cómo se va a seguir el mouse.

--- TP1/src/Servidor/modelo/ModeloScroll.h
+++ TP1/src/Servidor/modelo/ModeloScroll.h
@@ -1,9 +1,30 @@
 #pragma once
 
-class ModeloScroll{
+#include "../../utils/Observador/Observador.h"
+#include "../../utils/Observador/Observable.h"
+#include "../../utils/Constantes/Constantes.h"
+
+class ModeloScroll:public Observador{
+
+	private:
+		int x, y, ancho, alto;
+		int margen, velocidad;
+		void calcularPosicion(int mouseX, int mouseY);
 
 	public:
-		ModeloScroll(void);
+		ModeloScroll(int tAncho, int tAlto, int tMargen, int tVelocidad, class Observable *m);
 		~ModeloScroll(void);
 
+		void refresh(class Observable *m);
+		int getX();
+		int getY();
+		int getAlto();
+		void setAlto(int nuevoAlto);
+		void setAncho(int nuevoAncho);
+		int getAncho();
+		int getMargen();
+
+
 };
+
+
