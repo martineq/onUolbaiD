2ab75a74f9ef5ef24b8422577e11918327c80278
sanchezmatr <sanchezmatr@gmail.com>
2013-03-28 22:45:06 +0000

    Modifique hilo de movimiento para que se utilice la clase Hilo.

--- TP1/src/Servidor/modelo/ModeloEntidad.cpp
+++ TP1/src/Servidor/modelo/ModeloEntidad.cpp
@@ -2,66 +2,20 @@
 
 using namespace std;
 
-int ModeloEntidad::mover(void* objeto) {
-	Movimiento* movimiento = (Movimiento*)objeto;
-	ModeloEntidad* modeloEntidad = movimiento->modeloEntidad;
-	Posicion posicionDestino = movimiento->posicionDestino;
-	int deltaX = abs(posicionDestino.x - modeloEntidad->_posicionActual.x); 
-	int deltaY = abs(posicionDestino.y - modeloEntidad->_posicionActual.y);
-	int desplazamientoX = (modeloEntidad->_posicionActual.x < posicionDestino.x) ? 1 : -1;
-	int desplazamientoY = (modeloEntidad->_posicionActual.y < posicionDestino.y) ? 1 : -1;
-	int error = (deltaX >= deltaY) ? deltaX : deltaY;
-	int desplazamientoErrorX = 2 * deltaX;
-	int desplazamientoErrorY = 2 * deltaY;
-		
-	modeloEntidad->_posicionSiguiente = modeloEntidad->_posicionActual;
-
-	movimiento->ejecutando = true;
-	while (movimiento->ejecutando && (modeloEntidad->_posicionActual != posicionDestino)) {
-		Uint32 tiempoInicio = SDL_GetTicks();
-
-		modeloEntidad->_posicionSiguiente.x += (deltaX >= deltaY) ? desplazamientoX : 0;
-		modeloEntidad->_posicionSiguiente.y += (deltaX >= deltaY) ? 0 : desplazamientoY;
-		error += (deltaX >= deltaY) ? desplazamientoErrorY : desplazamientoErrorX; 
-		
-		if (deltaX >= deltaY) {
-			if (error > desplazamientoErrorX) {
-				modeloEntidad->_posicionSiguiente.y += desplazamientoY; 
-				error -= desplazamientoErrorX; 
-			}
-		}
-		else {
-			if (error > desplazamientoErrorY) {
-				modeloEntidad->_posicionSiguiente.x += desplazamientoX;
-				error -= desplazamientoErrorY;
-			}
-		}
-
-		modeloEntidad->notificarObservadores();
-		modeloEntidad->_posicionActual = modeloEntidad->_posicionSiguiente;
-		
-		Sleep(modeloEntidad->_velocidad);
-	}
-	return 0;
-}
-
 ModeloEntidad::ModeloEntidad(unsigned int alto, unsigned int ancho, unsigned int velocidad, Posicion posicion) {
 	this->_alto = alto;
 	this->_ancho = ancho;
 	this->_velocidad = velocidad;
 	this->_posicionActual = posicion;
 	this->_posicionSiguiente = posicion;
-
-	this->_movimientoActual.modeloEntidad = this;
-	this->_movimientoActual.posicionDestino = this->_posicionActual;
-	this->_movimientoActual.ejecutando = false;
-	this->_hiloMovimiento = NULL;
+	this->_movimientoActual = NULL;
 }
 
 ModeloEntidad::~ModeloEntidad() {
-	if (this->_hiloMovimiento != NULL) {
-		this->_movimientoActual.ejecutando = false;
-		SDL_WaitThread(this->_hiloMovimiento, NULL);
+	if (this->_movimientoActual != NULL) {
+		this->_movimientoActual->detener();
+		this->_movimientoActual->join();
+		delete this->_movimientoActual;
 	}
 }
 
@@ -90,13 +44,13 @@ Posicion ModeloEntidad::posicionSiguiente() const {
 }
 
 void ModeloEntidad::mover(Posicion posicionDestino) {
-	if (this->_hiloMovimiento != NULL) {
-		this->_movimientoActual.ejecutando = false;
-		SDL_WaitThread(this->_hiloMovimiento, NULL);
+	if (this->_movimientoActual != NULL) {
+		this->_movimientoActual->detener();
+		this->_movimientoActual->join();
+		delete this->_movimientoActual;
 	}
-	this->_movimientoActual.modeloEntidad = this;
-	this->_movimientoActual.posicionDestino = posicionDestino;
-	this->_hiloMovimiento = SDL_CreateThread(ModeloEntidad::mover, &this->_movimientoActual);
+	this->_movimientoActual = new Movimiento(this, posicionDestino);
+	this->_movimientoActual->start(NULL);
 }
 
 bool ModeloEntidad::operator==(const ModeloEntidad &modeloEntidad) const {
--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include "./Administrador.h"
-//#include <vld.h>
+#include <vld.h>
 
 int main(int argc, char* argv[]) {
 
