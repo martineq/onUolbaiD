680de5aa9fb6b92ab603ff96c673d5596382a1a9
martineq@gmail.com <martineq@gmail.com@3db02732-8920-3d1c-915d-0703c299baa4>
2013-03-29 17:58:50 +0000

    + Agrego los métodos del patrón observer a las clases que le corresponden.-

--- trunk/TP1/src/Cliente/vista/VistaScroll.cpp
+++ trunk/TP1/src/Cliente/vista/VistaScroll.cpp
@@ -7,3 +7,13 @@ VistaScroll::VistaScroll(void){
 VistaScroll::~VistaScroll(void){
 
 }
+
+void VistaScroll::actualizar(class Observable* s){
+
+	// En este punto ya se que el parámetro <s> se puede castear a ((ModeloScroll*)s)
+
+    // Actualizo datos, por ejemplo...
+	// this->posX = ((ModeloScroll*)s)->obtenerPosX();
+	//...completar con lo necesario
+
+}
