7fbdbfda05a65ba703f2eca21b2540297b946689
shamankingha <shamankingha@3db02732-8920-3d1c-915d-0703c299baa4>
2013-04-01 15:33:23 +0000

    * Agrego la SDLgfx, una biblioteca de SDL necesaria para agrandar o achicar las imágenes * Agrego la clase ImageLoader, que sirve para iniciar el SDL, cargar y expandir imágenes. * Agrego la clase SDLutil, que es la que va a contener las imágenes y los SDL_Rect para graficar. La clase automáticamente pone los objetos en la pantalla. * Agrego la variable SDL_IMAGEN_DEFAULT en Constantes.h

---
+++ TP1/src/Cliente/vista/ImageLoader.cpp
@@ -0,0 +1,89 @@
+/*
+ * ImageLoader.cpp
+ *
+ *  Created on: 08/09/2012
+ *      Author: Nestor
+ */
+
+#include "ImageLoader.h"
+
+ImageLoader::ImageLoader() {
+
+}
+
+ImageLoader::ImageLoader(const ImageLoader&){
+}
+
+
+SDL_Surface *ImageLoader::load_image( std::string filename )
+{
+	//The image that's loaded
+	SDL_Surface* loadedImage = NULL;
+
+	//The optimized surface that will be used
+	SDL_Surface* optimizedImage = NULL;
+
+	//Load the image
+	loadedImage = IMG_Load( filename.c_str() );
+
+	//If the image loaded
+	if( loadedImage == NULL ){
+		Log::getInstance().log(1,__FILE__,__LINE__,"No se cargará la siguiente imagen:");
+		Log::getInstance().log(1,__FILE__,__LINE__,filename);
+		loadedImage = IMG_Load( SDL_IMAGEN_DEFAULT );
+	}
+
+	//Create an optimized surface
+//	optimizedImage = SDL_DisplayFormatAlpha( loadedImage );
+	optimizedImage = SDL_DisplayFormatAlpha( loadedImage );
+
+	//Free the old surface
+	SDL_FreeSurface( loadedImage );
+
+	//If the surface was optimized
+	if( optimizedImage != NULL )
+	{
+		//Color key surface
+		SDL_SetColorKey( optimizedImage, SDL_SRCCOLORKEY, SDL_MapRGB( optimizedImage->format, 0, 0xFF, 0xFF ) );
+	}
+
+	//Return the optimized surface
+	return optimizedImage;
+}
+
+SDL_Surface* ImageLoader::stretch(SDL_Surface *surface, double width, double height)
+{	
+	double escalaAncho = width / surface->w;
+	double escalaAlto = height / surface->h;
+//	SDL_Surface * zoomed = rotozoomSurface (surface,0,2,1);
+	SDL_Surface * zoomed = zoomSurface (surface,escalaAncho,escalaAlto,1);
+	SDL_Surface * retornar = NULL;
+
+	retornar = SDL_DisplayFormatAlpha( zoomed );
+	SDL_FreeSurface(zoomed);
+	SDL_SetColorKey( retornar, SDL_SRCCOLORKEY, SDL_MapRGB( retornar->format, 0, 0xFF, 0xFF ) );
+	return retornar;
+}
+
+SDL_Rect ImageLoader::createRect(double x, double y){
+	SDL_Rect rect;
+	rect.x = x;
+	rect.y = y;
+	return rect;
+}
+
+bool ImageLoader::iniciarSDL(){
+	//Initialize all SDL subsystems
+	bool ok = true;
+/*	if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+	{
+		Log::getInstance().log(1,__FILE__,__LINE__,"SDL no se pudo iniciar");
+		ok = false;
+	}*/
+	return ok;
+}
+
+
+ImageLoader::~ImageLoader() {
+	// TODO Auto-generated destructor stub
+}
---
+++ TP1/src/Cliente/vista/SDLutil.cpp
@@ -0,0 +1,84 @@
+#include "SDLutil.h"
+
+SDLutil::SDLutil() {
+	this->mySurface = NULL;
+	this->area = NULL;
+}
+
+
+SDLutil::SDLutil(double x, double y, int w, int h, std::string filename){
+	//The image that's loaded
+	this->mySurface = ImageLoader::getInstance().load_image(filename);
+	this->mySurface = ImageLoader::getInstance().stretch(this->mySurface,w,h);
+
+	this->area = new SDL_Rect();
+	area->h = h;
+	area->w = w;
+	area->x = x;
+	area->y = y;
+	//Load the image
+}
+
+
+void SDLutil::setX(double x){
+	this->x = x;
+}
+
+void SDLutil::setY(double y){
+	this->y = y;
+}
+
+void SDLutil::setPantalla(SDL_Surface* pantalla){
+	this->screen = pantalla;
+}
+
+double SDLutil::getX(){
+	return this->x;
+}
+
+double SDLutil::getY(){
+	return this->y;
+}
+
+SDL_Surface* SDLutil::getSurface(){
+	return this->mySurface;
+}
+
+
+SDL_Rect* SDLutil::getRect(){
+	return this->area;
+}
+
+void SDLutil::graficar(){
+	int colorkey;
+    //Holds offsets
+	SDL_Rect offset;
+	offset.x = this->x;
+	offset.y = this->y;
+
+	colorkey = SDL_MapRGB(this->screen->format, 255, 0, 255);
+	SDL_SetColorKey(this->mySurface, SDL_SRCCOLORKEY | SDL_RLEACCEL, colorkey);
+	//SDL_DisplayFormat(source);	
+    //Blit
+	int res = SDL_BlitSurface( this->mySurface, this->area, this->screen, &offset );
+    if (res!=0) {
+    	Log::getInstance().log(1,__FILE__,__LINE__,"Error al agregar un componente a la pantalla");
+    }
+
+}
+
+void SDLutil::graficar(double x, double y){
+	this->x = x;
+	this->y = y;
+	this->graficar();
+}
+
+
+SDLutil::~SDLutil() {
+	if (this->mySurface!=NULL) {
+		SDL_FreeSurface(this->mySurface);
+	}	
+	if (this->area != NULL) {
+		delete this->area;
+	}
+}
