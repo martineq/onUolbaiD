072723afdec5c0ea0d356dc87163248c75b291c8
seba3789@gmail.com <seba3789@gmail.com@3db02732-8920-3d1c-915d-0703c299baa4>
2013-04-07 18:29:16 +0000

    Agrego método Static en ModeloScroll para ver si un punto (x,y) EN PIXELES está tocando o no los márgenes de la pantalla.
    Agregué getters en ModeloEvento, ya que la funcionalidad que antes tenía será implementada en ModeloLoop

--- trunk/TP1/src/Servidor/modelo/ModeloEvento.cpp
+++ trunk/TP1/src/Servidor/modelo/ModeloEvento.cpp
@@ -15,14 +15,31 @@ void ModeloEvento::actualizar(Observable* observable) {
 	//this->_modeloNivel->moverScroll(controladorEvento->getPosicionMouseX(), controladorEvento->getPosicionMouseY());
 
 	// Si se hizo clic con el boton derecho del mouse avisa al personaje que se mueva
-	if (controladorEvento->getClicMouseBotonDerecho()) {
-		int x = 0, y = 0;
-		Posicion::convertirPixelATile(this->_modeloNivel->getAltoTiles(), controladorEvento->getPosicionMouseX(), controladorEvento->getPosicionMouseY(), x, y);
+	//if (controladorEvento->getClicMouseBotonDerecho()) {
+		//int x = 0, y = 0;
+		//Posicion::convertirPixelATile(this->_modeloNivel->getAltoTiles(), controladorEvento->getPosicionMouseX(), controladorEvento->getPosicionMouseY(), x, y);
 		//TODO: implementar
 		//this->_modeloNivel->moverPersonaje(x, y);
-	}
+	//}
+
+	this->mouseClickDerecho = controladorEvento->getClicMouseBotonDerecho();
+	Posicion::convertirPixelATile(this->_modeloNivel->getAltoTiles(), controladorEvento->getPosicionMouseX(), controladorEvento->getPosicionMouseY(), this->mousePosX, this->mousePosY);
+
 }
 
 void ModeloEvento::asignarModeloNivel(ModeloNivel* modeloNivel) {
 	this->_modeloNivel = modeloNivel;
+}
+
+int ModeloEvento::getMouseClickDerecho() {
+	return this->mouseClickDerecho;
+}
+
+int ModeloEvento::getMousePosX() {
+	return this->mousePosX
+}
+
+
+int ModeloEvento::getMousePosY() {
+	return this->mousePosX
 }
