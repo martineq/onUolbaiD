2258ff5e609d5df3498ce4528aa3ec25e50477cc
sanchezmatr@gmail.com <sanchezmatr@gmail.com@3db02732-8920-3d1c-915d-0703c299baa4>
2013-04-11 21:52:38 +0000

    Agregue integracion de ModeloEntidad con ModeloNivel

--- trunk/TP1/src/main.cpp
+++ trunk/TP1/src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]) {
 
-	Administrador administrador;
+	//Administrador administrador;
 	//administrador.correrPruebas();
 	//administrador.iniciar();
 	//administrador.loop();
