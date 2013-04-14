8ff8f485b0d6dea2417422425d39ec07902bab5b
incorporado <incorporado@3db02732-8920-3d1c-915d-0703c299baa4>
2013-04-14 22:09:59 +0000

    + ParserYaml: Valida que el protagonista tenga 8 animaciones disponibles.-

--- trunk/TP1/src/utils/Constantes/Constantes.h
+++ trunk/TP1/src/utils/Constantes/Constantes.h
@@ -80,6 +80,7 @@
 #define YAML_PERSONAJE_LONGITUD_MAXIMA 50
 #define YAML_FPS_TOPE_MAXIMO 99
 #define YAML_DELAY_TOPE_MAXIMO 59
+#define YAML_CANTIDAD_OBLIGATORIA_DE_ANIMACIONES_PROTAGONISTA 8
 
 
 /*****************************  Habilitación de info de Mutex *************************/
