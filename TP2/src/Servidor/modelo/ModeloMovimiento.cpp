00f17143309b33cebbd0c414e0cafee5bcd39a11
incorporado <incorporado@3db02732-8920-3d1c-915d-0703c299baa4>
2013-04-20 18:26:47 +0000

    Added a folder remotely

---
+++ TP2/src/Servidor/modelo/ModeloMovimiento.cpp
@@ -0,0 +1,73 @@
+#include "ModeloEntidad.h"
+
+using namespace std;
+
+ModeloEntidad::ModeloMovimiento::ModeloMovimiento(const ModeloMovimiento &modeloMovimiento) {
+}
+
+ModeloEntidad::ModeloMovimiento& ModeloEntidad::ModeloMovimiento::operator=(const ModeloMovimiento &modeloMovimiento) {
+	return *this;
+}
+
+ModeloEntidad::ModeloMovimiento::ModeloMovimiento(ModeloEntidad* modeloEntidad) {
+	this->_modeloEntidad = modeloEntidad;
+    this->_posicionDestino = this->_modeloEntidad->_posicionActual;
+    this->_deltaX = 0;
+    this->_deltaY = 0;
+    this->_desplazamientoX = 0;
+    this->_desplazamientoY = 0;
+    this->_error = 0;
+    this->_desplazamientoErrorX = 0;
+    this->_desplazamientoErrorY = 0;
+    this->_instanteUltimoCambioEstado = 0;
+}
+
+ModeloEntidad::ModeloMovimiento::~ModeloMovimiento() {
+}
+
+void ModeloEntidad::ModeloMovimiento::actualizar(Posicion posicionDestino) {
+	this->_posicionDestino = posicionDestino;
+	this->_deltaX = abs(this->_posicionDestino.x - this->_modeloEntidad->_posicionActual.x);
+	this->_deltaY = abs(this->_posicionDestino.y - this->_modeloEntidad->_posicionActual.y);
+	this->_desplazamientoX = (this->_modeloEntidad->_posicionActual.x < this->_posicionDestino.x) ? 1 : -1;
+	this->_desplazamientoY = (this->_modeloEntidad->_posicionActual.y < this->_posicionDestino.y) ? 1 : -1;
+	this->_error = (this->_deltaX >= this->_deltaY) ? this->_deltaX : this->_deltaY;
+	this->_desplazamientoErrorX = 2 * this->_deltaX;
+	this->_desplazamientoErrorY = 2 * this->_deltaY;
+	this->_modeloEntidad->_posicionSiguiente = this->_modeloEntidad->_posicionActual;
+}
+
+void ModeloEntidad::ModeloMovimiento::cambiarEstado() {
+	if (this->_modeloEntidad->_posicionActual == this->_posicionDestino)
+		return;
+	
+	if (this->_instanteUltimoCambioEstado == 0) {
+		this->_instanteUltimoCambioEstado = GetTickCount();
+		return;
+	}
+
+	if (this->_modeloEntidad->velocidad() > (GetTickCount() - this->_instanteUltimoCambioEstado))
+		return;
+
+	this->_modeloEntidad->_posicionSiguiente.x += (this->_deltaX >= this->_deltaY) ? this->_desplazamientoX : 0;
+	this->_modeloEntidad->_posicionSiguiente.y += (this->_deltaX >= this->_deltaY) ? 0 : this->_desplazamientoY;
+	this->_error += (this->_deltaX >= this->_deltaY) ? this->_desplazamientoErrorY : this->_desplazamientoErrorX; 
+	
+	if (this->_deltaX >= this->_deltaY) {
+		if (this->_error > this->_desplazamientoErrorX) {
+			this->_modeloEntidad->_posicionSiguiente.y += this->_desplazamientoY; 
+			this->_error -= this->_desplazamientoErrorX; 
+		}
+	}
+	else {
+		if (this->_error > this->_desplazamientoErrorY) {
+			this->_modeloEntidad->_posicionSiguiente.x += this->_desplazamientoX;
+			this->_error -= this->_desplazamientoErrorY;
+		}
+	}
+
+	this->notificarObservadores();
+	this->_modeloEntidad->_posicionActual = this->_modeloEntidad->_posicionSiguiente;
+
+	this->_instanteUltimoCambioEstado = GetTickCount();
+}
---
+++ TP2/src/Servidor/modelo/VistaMovimiento.cpp
@@ -0,0 +1,133 @@
+#include "ModeloEntidad.h"
+
+using namespace std;
+
+Direccion ModeloEntidad::VistaMovimiento::obtenerDireccion(Posicion posicionOrigen, Posicion posicionDestino) {
+	if (posicionOrigen.x > posicionDestino.x) {
+		if (posicionOrigen.y > posicionDestino.y)
+			return NOROESTE;
+		else if (posicionOrigen.y < posicionDestino.y)
+			return SUDOESTE;
+		else
+			return OESTE;
+	}
+	else if (posicionOrigen.x < posicionDestino.x)
+		if (posicionOrigen.y > posicionDestino.y)
+			return NORESTE;
+		else if (posicionOrigen.y < posicionDestino.y)
+			return SUDESTE;
+		else
+			return ESTE;
+	else {
+		if (posicionOrigen.y > posicionDestino.y)
+			return NORTE;
+		else if (posicionOrigen.y < posicionDestino.y)
+			return SUR;
+		else
+			return SUR;
+	}
+}
+
+ModeloEntidad::VistaMovimiento::VistaMovimiento(const VistaMovimiento &modeloMovimiento) {
+}
+
+ModeloEntidad::VistaMovimiento& ModeloEntidad::VistaMovimiento::operator=(const VistaMovimiento &modeloMovimiento) {
+	return *this;
+}
+
+ModeloEntidad::VistaMovimiento::VistaMovimiento(ModeloEntidad* modeloEntidad, int altoMapa, int anchoMapa, int fps) {
+	this->_modeloEntidad = modeloEntidad;
+	this->_altoMapa = altoMapa;
+	this->_anchoMapa = anchoMapa;	
+	this->_cantidadCuadros = (this->_modeloEntidad->velocidad() * fps) / 1000;
+	
+	// Si la cantidad de cuadros a mostrar es 0 al menos muestro el cuadro final
+	if (this->_cantidadCuadros == 0)
+		this->_cantidadCuadros = 1;
+
+	this->_espera = this->_modeloEntidad->velocidad() / this->_cantidadCuadros;
+
+	this->_cuadroActual = this->_cantidadCuadros + 1;
+	this->_instanteUltimoCambioEstado = 0;
+}
+
+ModeloEntidad::VistaMovimiento::~VistaMovimiento() {
+}
+
+void ModeloEntidad::VistaMovimiento::actualizar(Observable* observable) {
+	// Calcula posiciones en pixeles
+	Posicion::convertirTileAPixel(this->_altoMapa, this->_modeloEntidad->posicionActual().x, this->_modeloEntidad->posicionActual().y, this->_posicionOrigen.x, this->_posicionOrigen.y);
+	Posicion::convertirTileAPixel(this->_altoMapa, this->_modeloEntidad->posicionSiguiente().x, this->_modeloEntidad->posicionSiguiente().y, this->_posicionDestino.x, this->_posicionDestino.y);
+
+	// Calcula desplazamientos
+	int deltaX = abs(this->_posicionDestino.x - this->_posicionOrigen.x);
+	int deltaY = abs(this->_posicionDestino.y - this->_posicionOrigen.y);
+	int desplazamientoX = (this->_posicionOrigen.x < this->_posicionDestino.x) ? 1 : -1;
+	int desplazamientoY = (this->_posicionOrigen.y < this->_posicionDestino.y) ? 1 : -1;
+	int error = (deltaX >= deltaY) ? deltaX : deltaY;
+	int desplazamientoErrorX = 2 * deltaX;
+	int desplazamientoErrorY = 2 * deltaY;
+	Posicion posicionActual = this->_posicionOrigen;
+
+	this->_posiciones.clear();
+
+	// Obtiene el camino pixel por pixel
+	while (posicionActual != this->_posicionDestino) {
+		posicionActual.x += (deltaX >= deltaY) ? desplazamientoX : 0;
+		posicionActual.y += (deltaX >= deltaY) ? 0 : desplazamientoY;
+		error += (deltaX >= deltaY) ? desplazamientoErrorY : desplazamientoErrorX; 
+		
+		if (deltaX >= deltaY) {
+			if (error > desplazamientoErrorX) {
+				posicionActual.y += desplazamientoY; 
+				error -= desplazamientoErrorX; 
+			}
+		}
+		else {
+			if (error > desplazamientoErrorY) {
+				posicionActual.x += desplazamientoX;
+				error -= desplazamientoErrorY;
+			}
+		}
+
+		this->_posiciones.push_back(posicionActual);
+	}
+
+	this->_desplazamiento = this->_posiciones.size() / this->_cantidadCuadros;
+	this->_cuadroActual = 0;
+}
+
+void ModeloEntidad::VistaMovimiento::cambiarEstado() {
+	if (this->_cuadroActual > this->_cantidadCuadros)
+		return;
+
+	if (this->_instanteUltimoCambioEstado == 0) {
+		this->_instanteUltimoCambioEstado = GetTickCount();
+		return;
+	}
+	
+	if (this->_espera > (GetTickCount() - this->_instanteUltimoCambioEstado))
+		return;
+
+	if (this->_cuadroActual == this->_cantidadCuadros) {
+		this->_modeloEntidad->_pixelSiguiente = this->_posicionDestino;
+		this->_modeloEntidad->_direccion = this->obtenerDireccion(this->_modeloEntidad->_pixelActual, this->_modeloEntidad->_pixelSiguiente);
+		this->_modeloEntidad->_esUltimoMovimiento = true;
+		this->_modeloEntidad->notificarObservadores();
+		this->_modeloEntidad->_pixelActual = this->_modeloEntidad->_pixelSiguiente;
+	}
+	else {
+		list<Posicion>::iterator iterador = this->_posiciones.begin();
+		
+		advance(iterador, this->_cuadroActual * this->_desplazamiento);
+
+		this->_modeloEntidad->_pixelSiguiente = *iterador;
+		this->_modeloEntidad->_direccion = this->obtenerDireccion(this->_modeloEntidad->_pixelActual, this->_modeloEntidad->_pixelSiguiente);
+		this->_modeloEntidad->_esUltimoMovimiento = false;
+		this->_modeloEntidad->notificarObservadores();
+		this->_modeloEntidad->_pixelActual = this->_modeloEntidad->_pixelSiguiente;
+	}
+
+	this->_cuadroActual++;
+	this->_instanteUltimoCambioEstado = GetTickCount();
+}
