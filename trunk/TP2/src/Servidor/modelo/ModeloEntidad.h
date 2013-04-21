eb9b36bf095ef042153ca7250b68e767cc736c84
sanchezmatr@gmail.com <sanchezmatr@gmail.com@3db02732-8920-3d1c-915d-0703c299baa4>
2013-04-21 16:35:50 +0000

    Se paso la clase Posicion a utils porque va a ser usada por el cliente y el servidor.

--- trunk/TP2/src/Servidor/modelo/ModeloEntidad.h
+++ trunk/TP2/src/Servidor/modelo/ModeloEntidad.h
@@ -8,44 +8,11 @@
 #include "../../utils/Constantes/Constantes.h"
 #include "../../utils/Observador/Observable.h"
 #include "../../utils/Observador/Identificable.h"
+#include "../../utils/Posicion/Posicion.h"
 #include "../../utils/Hilos/Hilo.h"
 #include "../../utils/Proxy/ProxyModeloEntidad.h"
 
 typedef enum Direccion { NORTE, NORESTE, ESTE, SUDESTE, SUR, SUDOESTE, OESTE, NOROESTE };
-
-typedef struct Posicion {
-	int x;
-	int y;
-
-	static void convertirTileAPixel(int altoEnTiles, int xTile, int yTile, int &xPixel, int &yPixel) {
-		xPixel = (ANCHO_TILE / 2) * (xTile - yTile) + (ANCHO_TILE / 2) * altoEnTiles;
-		yPixel = (ALTO_TILE / 2) * (xTile + yTile);
-	}
-
-	static void convertirPixelATile(int altoEnTiles, int xPixel, int yPixel, int &xTile, int &yTile) {
-		double desplazamientoX = 0, desplazamientoY = 0;
-		
-		xPixel -= (ANCHO_TILE / 2) * altoEnTiles;
-		desplazamientoX = (double)xPixel / ANCHO_TILE;
-		desplazamientoY = (double)yPixel / ALTO_TILE;
-		
-		xTile = floor(desplazamientoY + desplazamientoX);
-		yTile = floor(desplazamientoY - desplazamientoX);
-	} 
-
-	Posicion() {
-		this->x = 0;
-		this->y = 0;
-	}
-
-	bool operator==(const Posicion &posicion) const {
-		return ((this->x == posicion.x) && (this->y == posicion.y));
-	}
-
-	bool operator!=(const Posicion &posicion) const {
-		return !(*this == posicion);
-	}
-} Posicion;
 
 class ModeloEntidad : public Observable, public Identificable {
 	private:
