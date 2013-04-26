5b14a0f08de4e913a9e628986739a1ce23e78ff5
sanchezmatr <sanchezmatr@gmail.com>
2013-04-26 01:26:54 +0000

    Se paso Posicion a una libreira aparte

---
+++ TP2_SIN_SOCKETS/src/utils/Posicion/Posicion.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <math.h>
+
+#include "../Constantes/Constantes.h"
+
+typedef struct Posicion {
+	int x;
+	int y;
+
+	static void convertirTileAPixel(int altoEnTiles, int xTile, int yTile, int &xPixel, int &yPixel) {
+		xPixel = (ANCHO_TILE / 2) * (xTile - yTile) + (ANCHO_TILE / 2) * altoEnTiles;
+		yPixel = (ALTO_TILE / 2) * (xTile + yTile);
+	}
+
+	static void convertirPixelATile(int altoEnTiles, int xPixel, int yPixel, int &xTile, int &yTile) {
+		double desplazamientoX = 0, desplazamientoY = 0;
+		
+		xPixel -= (ANCHO_TILE / 2) * altoEnTiles;
+		desplazamientoX = (double)xPixel / ANCHO_TILE;
+		desplazamientoY = (double)yPixel / ALTO_TILE;
+		
+		xTile = floor(desplazamientoY + desplazamientoX);
+		yTile = floor(desplazamientoY - desplazamientoX);
+	} 
+
+	Posicion() {
+		this->x = 0;
+		this->y = 0;
+	}
+
+	bool operator==(const Posicion &posicion) const {
+		return ((this->x == posicion.x) && (this->y == posicion.y));
+	}
+
+	bool operator!=(const Posicion &posicion) const {
+		return !(*this == posicion);
+	}
+} Posicion;
