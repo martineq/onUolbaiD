15de794ad4964c6463f9c78e598a056431d75a4b
sanchezmatr@gmail.com <sanchezmatr@gmail.com@3db02732-8920-3d1c-915d-0703c299baa4>
2013-05-02 21:09:40 +0000

    Agregue nombre de remitente y destinatario al chat

--- trunk/TP2_SIN_SOCKETS/src/Cliente/vista/VistaChat.cpp
+++ trunk/TP2_SIN_SOCKETS/src/Cliente/vista/VistaChat.cpp
@@ -1,20 +1,34 @@
 #include "VistaChat.h"
 
-#define ALTO_MENSAJE 14
-#define ANCHO_MENSAJE 220
-#define CANTIDAD_MAXIMA_CARACTERES 20
+//TODO: Ver de usar esta funcion para mejorar el tema de los tamaños
+//TTF_SizeText
+
+#define MARGEN 5
+#define ALTO_LETRA 14
+#define ANCHO_LETRA 11
+#define CANTIDAD_MAXIMA_CARACTERES_DESTINATARIO 10
+#define CANTIDAD_MAXIMA_CARACTERES_MENSAJE 55
 #define CANTIDAD_MAXIMA_MENSAJES 10
-#define SEPARACION 5
+
+#define ALTO_TEXTO_MENSAJES (ALTO_LETRA * CANTIDAD_MAXIMA_MENSAJES)
+#define ANCHO_TEXTO_MENSAJES (ANCHO_LETRA * (CANTIDAD_MAXIMA_CARACTERES_DESTINATARIO + MARGEN + CANTIDAD_MAXIMA_CARACTERES_MENSAJE))
+
+#define ALTO_TEXTO_INGRESADO (ALTO_LETRA)
+#define ANCHO_TEXTO_INGRESADO (ANCHO_LETRA * CANTIDAD_MAXIMA_CARACTERES_MENSAJE)
+
+#define ALTO_VENTANA (ALTO_TEXTO_MENSAJES + ALTO_TEXTO_INGRESADO + (3 * MARGEN))
+#define ANCHO_VENTANA (ANCHO_TEXTO_MENSAJES + (2 * MARGEN))
 
 using namespace std;
 
-VistaChat::VistaChat(Posicion posicion) {
+VistaChat::VistaChat(Posicion posicion, string remitente) {
 	this->_posicion = posicion;
+	this->_remitente = remitente;
 	this->_visible = false;
 	this->_fuente = TTF_OpenFont("verdana.ttf", 10);
-	this->_ventana = SDL_CreateRGBSurface(SDL_SWSURFACE, ANCHO_MENSAJE + (2 * SEPARACION), ((CANTIDAD_MAXIMA_MENSAJES + 1) * ALTO_MENSAJE) + (3 * SEPARACION), 32, 0, 0, 0, 0);
-	this->_cajaTextoChat = SDL_CreateRGBSurface(SDL_SWSURFACE, ANCHO_MENSAJE, CANTIDAD_MAXIMA_MENSAJES * ALTO_MENSAJE, 32, 0, 0, 0, 0);
-	this->_cajaTextoIngresado = SDL_CreateRGBSurface(SDL_SWSURFACE, ANCHO_MENSAJE, ALTO_MENSAJE, 32, 0, 0, 0, 0);
+	this->_ventana = SDL_CreateRGBSurface(SDL_SWSURFACE, ANCHO_VENTANA, ALTO_VENTANA, 32, 0, 0, 0, 0);
+	this->_cajaTextoMensajes = SDL_CreateRGBSurface(SDL_SWSURFACE, ANCHO_TEXTO_MENSAJES, ALTO_TEXTO_MENSAJES, 32, 0, 0, 0, 0);
+	this->_cajaTextoIngresado = SDL_CreateRGBSurface(SDL_SWSURFACE, ANCHO_TEXTO_INGRESADO, ALTO_TEXTO_INGRESADO, 32, 0, 0, 0, 0);
 }
 
 VistaChat::~VistaChat() {
@@ -22,32 +36,38 @@ VistaChat::~VistaChat() {
 		TTF_CloseFont(this->_fuente);
 	if (this->_ventana != NULL)
 		SDL_FreeSurface(this->_ventana);
-	if (this->_cajaTextoChat != NULL)
-		SDL_FreeSurface(this->_cajaTextoChat);
+	if (this->_cajaTextoMensajes != NULL)
+		SDL_FreeSurface(this->_cajaTextoMensajes);
 	if (this->_cajaTextoIngresado != NULL)
 		SDL_FreeSurface(this->_cajaTextoIngresado);
 }
 
-void VistaChat::agregarMensaje(std::string usuario, std::string mensaje) {
+void VistaChat::agregarMensaje(string remitente, string mensaje) {
 	if (this->_mensajes.size() == 10)
 		this->_mensajes.pop_front();
-	string cadena = usuario + ": " + mensaje;
-	this->_mensajes.push_back(cadena.substr(0, CANTIDAD_MAXIMA_CARACTERES));
+	string cadena = remitente + ": " + mensaje;
+	this->_mensajes.push_back(cadena.substr(0, CANTIDAD_MAXIMA_CARACTERES_MENSAJE));
+}
+
+void VistaChat::asignarDestinatario(std::string destinatario) {
+	this->_destinatario = destinatario;
 }
 
 bool VistaChat::graficar(SDL_Surface* pantalla) {
 	if (!this->_visible)
 		return true;
 
-	if ((this->_fuente == NULL) || (this->_ventana == NULL) || (this->_cajaTextoChat == NULL) || (this->_cajaTextoIngresado == NULL))
+	if ((this->_fuente == NULL) || (this->_ventana == NULL) || (this->_cajaTextoMensajes == NULL) || (this->_cajaTextoIngresado == NULL))
 		return false;
 	
 	SDL_Rect posicionDestino;
 	SDL_Color colorTexto = { 0, 0, 0 };
 	SDL_Color colorFondo = { 255, 255, 255 };
 	SDL_Surface* textoIngresado = TTF_RenderText_Shaded(this->_fuente, this->_textoIngresado.c_str(), colorTexto, colorFondo);
+	SDL_Surface* textoDestinatario = TTF_RenderText_Blended(this->_fuente, (this->_destinatario.substr(0, CANTIDAD_MAXIMA_CARACTERES_DESTINATARIO) + ":").c_str(), colorTexto);
 	int contadorMensajes = 0;
 	
+	// Muestra la ventana completa
 	posicionDestino.h = this->_ventana->h;
 	posicionDestino.w = this->_ventana->w;
 	posicionDestino.x = this->_posicion.x;
@@ -56,42 +76,52 @@ bool VistaChat::graficar(SDL_Surface* pantalla) {
 	SDL_FillRect(this->_ventana, NULL, SDL_MapRGB(this->_cajaTextoIngresado->format, 255, 0, 0));
 	SDL_BlitSurface(this->_ventana, NULL, pantalla, &posicionDestino);
 
-	posicionDestino.h = this->_cajaTextoChat->h;
-	posicionDestino.w = this->_cajaTextoChat->w;
-	posicionDestino.x = this->_posicion.x + SEPARACION;
-	posicionDestino.y = this->_posicion.y + SEPARACION;
+	posicionDestino.h = this->_cajaTextoMensajes->h;
+	posicionDestino.w = this->_cajaTextoMensajes->w;
+	posicionDestino.x = this->_posicion.x + MARGEN;
+	posicionDestino.y = this->_posicion.y + MARGEN;
 
-	SDL_FillRect(this->_cajaTextoChat, NULL, SDL_MapRGB(this->_cajaTextoIngresado->format, 255, 255, 255));
-	SDL_BlitSurface(this->_cajaTextoChat, NULL, pantalla, &posicionDestino);
+	// Muestra los mensajes
+	SDL_FillRect(this->_cajaTextoMensajes, NULL, SDL_MapRGB(this->_cajaTextoIngresado->format, 255, 255, 255));
+	SDL_BlitSurface(this->_cajaTextoMensajes, NULL, pantalla, &posicionDestino);
 
 	for (list<string>::iterator mensaje = this->_mensajes.begin(); mensaje != this->_mensajes.end(); mensaje++) {
 		SDL_Surface* textoMensaje = TTF_RenderText_Shaded(this->_fuente, (*mensaje).c_str(), colorTexto, colorFondo);
 		if (textoMensaje != NULL) {
 			posicionDestino.h = textoMensaje->h;
 			posicionDestino.w = textoMensaje->w;
-			posicionDestino.y = this->_posicion.y + SEPARACION + (contadorMensajes++ * 14);
+			posicionDestino.y = this->_posicion.y + MARGEN + (contadorMensajes++ * 14);
 
 			SDL_BlitSurface(textoMensaje, NULL, pantalla, &posicionDestino);
 			SDL_FreeSurface(textoMensaje);
 		}
 	}
 
+	// Muestro destinatario
+	if (textoDestinatario != NULL) {
+		posicionDestino.h = textoDestinatario->h;
+		posicionDestino.w = textoDestinatario->w;
+		posicionDestino.x = this->_posicion.x + MARGEN;
+		posicionDestino.y = this->_posicion.y + this->_cajaTextoMensajes->h + (2 * MARGEN);
+		SDL_BlitSurface(textoDestinatario, NULL, pantalla, &posicionDestino);
+		SDL_FreeSurface(textoDestinatario);
+	}
+	
+	// Muestro el mensaje ingresado
 	posicionDestino.h = this->_cajaTextoIngresado->h;
 	posicionDestino.w = this->_cajaTextoIngresado->w;
-	posicionDestino.x = this->_posicion.x + SEPARACION;
-	posicionDestino.y = this->_posicion.y + this->_cajaTextoChat->h + (2 * SEPARACION);
+	posicionDestino.x = this->_posicion.x + this->_ventana->w - MARGEN - this->_cajaTextoIngresado->w;
+	posicionDestino.y = this->_posicion.y + this->_cajaTextoMensajes->h + (2 * MARGEN);
 
 	SDL_FillRect(this->_cajaTextoIngresado, NULL, SDL_MapRGB(this->_cajaTextoIngresado->format, 255, 255, 255));
 	SDL_BlitSurface(this->_cajaTextoIngresado, NULL, pantalla, &posicionDestino);
 
-	if (textoIngresado == NULL)
-		return true;
-
-	posicionDestino.h = textoIngresado->h;
-	posicionDestino.w = textoIngresado->w;
-
-	SDL_BlitSurface(textoIngresado, NULL, pantalla, &posicionDestino);
-	SDL_FreeSurface(textoIngresado);
+	if (textoIngresado != NULL) {
+		posicionDestino.h = textoIngresado->h;
+		posicionDestino.w = textoIngresado->w;
+		SDL_BlitSurface(textoIngresado, NULL, pantalla, &posicionDestino);
+		SDL_FreeSurface(textoIngresado);
+	}
 	
 	return true;
 }
@@ -99,7 +129,7 @@ bool VistaChat::graficar(SDL_Surface* pantalla) {
 void VistaChat::teclaPresionada(Uint16 tecla) {
 	// Si presiono enter agrego el mensaje a la lista
 	if ((tecla == SDLK_RETURN) && (this->_textoIngresado.length() > 0)) {
-		this->agregarMensaje("yo", this->_textoIngresado);
+		this->agregarMensaje(this->_remitente, this->_textoIngresado);
 		this->_textoIngresado.clear();
 		return;
 	}
@@ -111,7 +141,7 @@ void VistaChat::teclaPresionada(Uint16 tecla) {
 	}
 
 	// Si alcanzo la cantidad maxima de caracteres salgo
-	if (this->_textoIngresado.length() >= CANTIDAD_MAXIMA_CARACTERES)
+	if (this->_textoIngresado.length() >= CANTIDAD_MAXIMA_CARACTERES_MENSAJE)
 		return;
 	
 	// Si presiono un caracter valido lo agrego a la cadena
--- trunk/TP2_SIN_SOCKETS/src/Cliente/vista/VistaChat.h
+++ trunk/TP2_SIN_SOCKETS/src/Cliente/vista/VistaChat.h
@@ -11,20 +11,24 @@
 class VistaChat {
 	private:
 		Posicion _posicion;
+		std::string _remitente;
+		std::string _destinatario;
 		bool _visible;
 		std::list<std::string> _mensajes;
 		std::string _textoIngresado;
 		TTF_Font* _fuente;
 		SDL_Surface* _ventana;
-		SDL_Surface* _cajaTextoChat;
+		SDL_Surface* _cajaTextoMensajes;
 		SDL_Surface* _cajaTextoIngresado;
 
 	public:
-		VistaChat(Posicion posicion);
+		VistaChat(Posicion posicion, std::string remitente);
 
 		virtual ~VistaChat();
 
-		void agregarMensaje(std::string usuario, std::string mensaje);
+		void agregarMensaje(std::string remitente, std::string mensaje);
+
+		void asignarDestinatario(std::string destinatario);
 
 		bool graficar(SDL_Surface* pantalla);
 
