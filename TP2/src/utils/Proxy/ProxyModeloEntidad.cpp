6be470f8e2c230b22e9df6dc5ab577bb1188cd8a
martineq <martineq@gmail.com>
2013-05-04 03:46:56 +0000

    + Actualizo los métodos de los proxys.-

--- TP2/src/utils/Proxy/ProxyModeloEntidad.cpp
+++ TP2/src/utils/Proxy/ProxyModeloEntidad.cpp
@@ -1,13 +1,30 @@
 #include "ProxyModeloEntidad.h"
 
 ProxyModeloEntidad::ProxyModeloEntidad(void){
-
+	this->pCliente = NULL;
+	this->pServidor = NULL;
 }
 
 ProxyModeloEntidad::~ProxyModeloEntidad(void){
 
 }
 
-void ProxyModeloEntidad::cambiarEstado(){
-	// TODO: usar this->notificarObservadores();
+void ProxyModeloEntidad::setSocketCliente(SocketCliente* pCliente){
+	this->pCliente = pCliente;
+}
+
+void ProxyModeloEntidad::setSocketServidor(SocketServidor* pServidor){
+	this->pServidor = pServidor;
+}
+
+bool ProxyModeloEntidad::enviarEntidad(ProxyModeloEntidad::stEntidad entidad){
+	// TODO: Implementar
+	// Acá uso this->pCliente;
+	return true;
+}
+
+bool ProxyModeloEntidad::recibirEntidad(ProxyModeloEntidad::stEntidad& entidad){
+	// TODO: Implementar
+	// Acá uso this->pCliente = NULL;
+	return true;
 }
--- TP2/src/utils/Proxy/ProxyModeloEntidad.h
+++ TP2/src/utils/Proxy/ProxyModeloEntidad.h
@@ -1,12 +1,31 @@
 #pragma once
 
-#include "../Observador/Observable.h"
+#include "../Sockets/SocketCliente.h"
+#include "../Sockets/SocketServidor.h"
 
-class ProxyModeloEntidad: public Observable{
-	
+class ProxyModeloEntidad{
+		
+	public:
+		struct stEntidad{
+			int id;
+			double pixelSiguienteX;
+			double pixelSiguienteY;
+			int direccion;
+			bool esUltimoMovimiento;
+	};
+
+	private:
+		SocketCliente* pCliente;
+		SocketServidor* pServidor;
+
 	public:
 		ProxyModeloEntidad(void);
 		~ProxyModeloEntidad(void);
-		
-		void cambiarEstado();
+
+		void setSocketCliente(SocketCliente* pCliente);
+		void setSocketServidor(SocketServidor* pServidor);
+		bool enviarEntidad(ProxyModeloEntidad::stEntidad entidad);
+		bool recibirEntidad(ProxyModeloEntidad::stEntidad& entidad);
 };
+
+// TODO: Implementar todos los métodos necesarios para la parte de cliente y servidor
