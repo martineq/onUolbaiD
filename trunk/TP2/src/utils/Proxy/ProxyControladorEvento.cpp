cf4d98824883a1e92d39177fefc68a834ebcae74
martineq@gmail.com <martineq@gmail.com@3db02732-8920-3d1c-915d-0703c299baa4>
2013-05-04 03:46:56 +0000

    + Actualizo los métodos de los proxys.-

--- trunk/TP2/src/utils/Proxy/ProxyControladorEvento.cpp
+++ trunk/TP2/src/utils/Proxy/ProxyControladorEvento.cpp
@@ -1,13 +1,31 @@
 #include "ProxyControladorEvento.h"
 
 ProxyControladorEvento::ProxyControladorEvento(void){
-
+	this->pCliente = NULL;
+	this->pServidor = NULL;
 }
 
 ProxyControladorEvento::~ProxyControladorEvento(void){
 
 }
 
-void ProxyControladorEvento::cambiarEstado(){
-	// TODO: usar this->notificarObservadores();
+void ProxyControladorEvento::setSocketCliente(SocketCliente* pCliente){
+	this->pCliente = pCliente;
 }
+
+void ProxyControladorEvento::setSocketServidor(SocketServidor* pServidor){
+	this->pServidor = pServidor;
+}
+
+bool ProxyControladorEvento::enviarEvento(ProxyControladorEvento::stEvento entidad){
+	// TODO: Implementar
+	// Acá uso this->pCliente;
+	return true;
+}
+
+bool ProxyControladorEvento::recibirEvento(ProxyControladorEvento::stEvento& evento){
+	// TODO: Implementar
+	// Acá uso this->pServidor;
+	return true;
+}
+
--- trunk/TP2/src/utils/Proxy/ProxyControladorEvento.h
+++ trunk/TP2/src/utils/Proxy/ProxyControladorEvento.h
@@ -1,12 +1,32 @@
 #pragma once
 
-#include "../Observador/Observable.h"
+#include "../Sockets/SocketCliente.h"
+#include "../Sockets/SocketServidor.h"
 
-class ProxyControladorEvento: public Observable{
+class ProxyControladorEvento{
 
+	public:
+		struct stEvento{
+			int id;
+			int mouseX;
+			int mouseY;
+			bool mouseClickIzquierdo;
+			bool mouseDentroPantalla;
+	};
+
+	private:
+		SocketCliente* pCliente;
+		SocketServidor* pServidor;
+	
 	public:
 		ProxyControladorEvento(void);
 		~ProxyControladorEvento(void);
+
+		void setSocketCliente(SocketCliente* pCliente);
+		void setSocketServidor(SocketServidor* pServidor);
+		bool enviarEvento(ProxyControladorEvento::stEvento entidad);
+		bool recibirEvento(ProxyControladorEvento::stEvento& evento);
 		
-		void cambiarEstado();
 };
+
+// TODO: Implementar todos los métodos necesarios para la parte de cliente y servidor
