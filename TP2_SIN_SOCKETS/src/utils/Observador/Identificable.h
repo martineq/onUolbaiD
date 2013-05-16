e672b77f2af62a4af31a2c4818d729389d81434b
martineq <martineq@gmail.com>
2013-05-16 18:36:09 +0000

    Borro la carpeta TP2 sin socktes.-

--- TP2_SIN_SOCKETS/src/utils/Observador/Identificable.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Interfaz para observadores y observables que necesiten tener id para reconocerse
-class Identificable {
-	public:
-		virtual int id() const = 0;
-		
-		virtual ~Identificable() { }
-};
--- TP2_SIN_SOCKETS/src/utils/Observador/Observador.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Interfaz para elementos que deseen ser observadores, de un objeto <Observable>
-class Observador{
-public:
-	virtual void actualizar(class Observable* s) = 0;
-	virtual ~Observador(){}
-};
