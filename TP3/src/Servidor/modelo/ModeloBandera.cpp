b40590fffbb02d161db7b8fe68d09adc3da4a6ae
sanchezmatr <sanchezmatr@gmail.com>
2013-06-09 20:59:14 +0000

    Subo archivos para que compile commit anterior

---
+++ TP3/src/Servidor/modelo/ModeloBandera.cpp
@@ -0,0 +1,14 @@
+#include "ModeloItem.h"
+
+using namespace std;
+
+bool ModeloBandera::aplicar(ModeloJugador* jugador, ListaJugadores* listaJugadores, ListaJugadores* listaEnemigos, ListaJugadores* listaGolems) {
+	return true;
+}
+
+ModeloBandera::ModeloBandera(int alto, int ancho, int velocidad, Posicion posicion, int altoNivel, int anchoNivel, int fps, ProxyModeloEntidad* proxyEntidad, int id, string nombreEntidad)
+	: ModeloItem(alto, ancho, velocidad, posicion, altoNivel, anchoNivel, fps, proxyEntidad, id, nombreEntidad, TIPO_ITEM_BANDERA) {
+}
+
+ModeloBandera::~ModeloBandera() {
+}
